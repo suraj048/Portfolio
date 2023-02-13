f3dccb417c5d5a89f3abed2c04ab4c6c6dc2bcf0
Suraj048 <suraj.papu011@gmail.com>
2023-02-13 19:03:51 +0530

    Changed js icon margin

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -46,7 +46,7 @@ const Skills = () => {
               <div className="m-auto">
                 <Image src={Javascript} width={64} height={64} alt="/" />
               </div>
-              <div className="flex flex-col items-center justify-center pl-2">
+              <div className="flex flex-col items-center justify-center pl-2 ml-2 sm:ml-auto">
                 <h3>JavaScript</h3>
               </div>
             </div>
