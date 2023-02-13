e7b95138602cb41a9dc79de3cfd0e8ac8fbae8de
Suraj048 <suraj.papu011@gmail.com>
2023-02-13 19:06:18 +0530

    changed JS Icon Margin

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -43,7 +43,7 @@ const Skills = () => {
           </div>
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
+              <div className="mr-2 sm:m-auto">
                 <Image src={Javascript} width={64} height={64} alt="/" />
               </div>
               <div className="flex flex-col items-center justify-center pl-2 ml-2 sm:ml-auto">
